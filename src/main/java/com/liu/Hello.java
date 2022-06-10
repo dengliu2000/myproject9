c61820863331161a2546ba8bf8f1e242121dc5e0
DESKTOP-OA6FUQH\nick <dengliu2000@gmail.com>
2022-06-10 15:03:31 +0800

    Hello world with class Person

--- src/main/java/com/liu/Hello.java
+++ src/main/java/com/liu/Hello.java
@@ -1,5 +1,9 @@
 package com.liu;
 
 public class Hello {
-
+    public static void main(String[] args) {
+        //System.out.println("Hello world");
+        Person p=new Person();
+        p.greeting();
+    }
 }
