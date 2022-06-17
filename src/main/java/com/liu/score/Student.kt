573fbad4bc20ecb54604b4b9017d819d5b21a8b3
DESKTOP-OA6FUQH\nick <dengliu2000@gmail.com>
2022-06-17 15:06:44 +0800

    Student Kotlin

--- src/main/java/com/liu/score/Student.kt
+++ src/main/java/com/liu/score/Student.kt
@@ -1,11 +1,18 @@
 package com.liu.com.liu.score
 
-class Student {
-    var name=""
-    var english=0
-    var math=0
+class Student(var name:String,var english:Int,var math:Int) {
+    init {
+        //println("Test")
+    }
+    fun print(){
+        println("$name\t$english\t$math\t${getAverage()}")
+    }
+    fun getAverage():Int{
+        return (english+math)/2
+    }
 }
 
 fun main() {
     val stu=Student("Frank",55,87)
+    stu.print()
 }
